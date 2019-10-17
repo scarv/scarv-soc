5ccbae355977949fbb5f9da00b0f32147131aae4
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-10-17 12:17:25 +0100

    Fix up project workspace script
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   bin/conf.sh
    
     Changes not staged for commit:
    	modified:   doc/soc-design.md
    
     Untracked files:
    	Makefile
    	doc/memory-interconnect.md
    	flow/
    	rtl/
    	verif/
    	work/

--- bin/conf.sh
+++ bin/conf.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 export SOC_HOME="${PWD}"
+export SOC_WORK=$SOC_HOME/work
+
+if [[ -z "$VERILATOR_ROOT" ]]; then
+    export VERILATOR_ROOT=/home/ben/tools/verilator
+fi
+
+echo "------------------------ SCARV SoC Project Setup ----------------------"
+echo "SOC_HOME       = $SOC_HOME"
+echo "SOC_WORK       = $SOC_WORK"
+echo "VERILATOR_ROOT = $VERILATOR_ROOT"
+echo "-----------------------------------------------------------------------"
 
