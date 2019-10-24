d99c12f5b358b22c565a11c8570362cb1d3b0fdd
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-10-24 09:33:02 +0100

    Split data error tests
    - Rename data bus error test to data load error
    - New test for data store errors
    
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   flow/gtkwave/scarv-soc.gtkw
    	renamed:    verif/selfcheck/test_03_data_bus_error/error.S -> verif/selfcheck/test_03_data_load_error/error.S
    	renamed:    verif/selfcheck/test_03_data_bus_error/main.c -> verif/selfcheck/test_03_data_load_error/main.c
    	new file:   verif/selfcheck/test_04_data_store_error/error.S
    	new file:   verif/selfcheck/test_04_data_store_error/main.c

---
+++ verif/selfcheck/test_04_data_store_error/main.c
@@ -0,0 +1,58 @@
+
+#include "stddef.h"
+
+#include "selfcheck.h"
+
+#define ALEN 20
+
+/*!
+@brief Checks if a specified address correctly triggers 
+a data load trap or not
+*/
+extern int test_data_store_error(
+    char * addr,
+    char   expect_error
+);
+
+typedef struct {
+    char * addr;
+    char   expect_error;
+} test_spec_t;
+
+test_spec_t tests [] = {
+    {.addr = (char*)0x90000000, .expect_error = 1},
+    {.addr = (char*)0xA0000000, .expect_error = 1},
+    {.addr = (char*)0x50000000, .expect_error = 1},
+    {.addr = (char*)0x3FFFFFFF, .expect_error = 1},
+    {.addr = (char*)0x20010000, .expect_error = 1},
+    {.addr = (char*)0x2000FFFF, .expect_error = 0},
+    {.addr = (char*)0x20000000, .expect_error = 0},
+    {.addr = (char*)0x1FFFFFFF, .expect_error = 1},
+    {.addr = (char*)0x10000400, .expect_error = 1},
+    {.addr = (char*)0x100003FF, .expect_error = 0},
+    {.addr = (char*)0x10000000, .expect_error = 0},
+    {.addr = (char*)0x0FFFFFFF, .expect_error = 1} 
+};
+
+int main() {
+
+    int result;
+  
+    size_t num_tests = sizeof(tests) / sizeof(test_spec_t);
+
+    for(int i = 0; i < num_tests; i ++) {
+
+        result = test_data_store_error(
+            tests[i].addr,
+            tests[i].expect_error
+        );
+
+        if(result) {
+            return SELFCHECK_FAIL;
+        }
+
+    }
+    
+
+    return SELFCHECK_PASS;
+}
