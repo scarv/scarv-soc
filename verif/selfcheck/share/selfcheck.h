29509d17de936caa9c8e34dac95495f6cce0724c
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-10-25 14:12:38 +0100

    Sparse test for the modelled AXI UART.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   flow/selfcheck/Makefile.in
    	modified:   verif/selfcheck/share/selfcheck.S
    	modified:   verif/selfcheck/share/selfcheck.h
    	new file:   verif/selfcheck/test_07_axi_uart/main.c
    
     Changes not staged for commit:
    	modified:   rtl/ic/ic_cpu_bus_axi_bridge.v

--- verif/selfcheck/share/selfcheck.h
+++ verif/selfcheck/share/selfcheck.h
@@ -8,5 +8,11 @@
 //! Failure code returned to the selfcheck tests bootloader.
 #define SELFCHECK_FAIL 1
 
+/*!
+@brief Write a single character to the simulated UART AXI device.
+@note Does not block
+*/
+void selfcheck_uart_putc(char tx);
+
 #endif
 
---
+++ verif/selfcheck/test_07_axi_uart/main.c
@@ -0,0 +1,17 @@
+
+
+#include "selfcheck.h"
+
+char * message = "Hello World!\n"
+                 "This is the SCARV-CPU Talking.\n"
+                 "The simulated UART Works :)\n";
+
+int main() {
+
+    for(int i = 0; message[i] != 0; i ++) {
+        selfcheck_uart_putc(message[i]);
+    }
+
+    return SELFCHECK_PASS;   
+
+}
