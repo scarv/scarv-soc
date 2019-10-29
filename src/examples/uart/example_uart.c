d18032b92e4dd2efc306dff8514954f6f8c3956e
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-10-29 14:01:32 +0000

    UART Example: print welcome message

--- src/examples/uart/example_uart.c
+++ src/examples/uart/example_uart.c
@@ -1,12 +1,15 @@
 
 #include "scarvsoc.h"
 
+char * start_message  = "UART Echo Example Program\n"    ;
 char * finish_message = "\nRecieved Finish Byte. Stop.\n";
 
 /*!
 @brief A simple test program which echoes recieved UART bytes.
 */
 int main(int argc, char ** argv) {
+    
+    scarvsoc_uart_putstr_b(SCARVSOC_UART0, start_message);
     
     char recv = scarvsoc_uart_getc_b(SCARVSOC_UART0);
 
