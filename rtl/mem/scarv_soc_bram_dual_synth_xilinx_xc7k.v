387242c3b90a547d0a045082fa4d86bb4c03d7df
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-10-30 11:18:02 +0000

    Re-write Xilinx bram module
    - Easier to work with init mem files.
    - Coppies their language template for BRAM inference.
    
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   rtl/mem/scarv_soc_bram_dual_synth_xilinx_xc7k.v
    
     Changes not staged for commit:
    	modified:   src/fsbl/Makefile.in
    
     Untracked files:
    	bin/vl-hex-align.py

--- rtl/mem/scarv_soc_bram_dual_synth_xilinx_xc7k.v
+++ rtl/mem/scarv_soc_bram_dual_synth_xilinx_xc7k.v
@@ -34,151 +34,59 @@ localparam  LW    = $clog2(DEPTH);
 //! Memory file to load
 parameter [255*8:0] MEMH_FILE = "NONE";
 
-wire [3:0] wen_a = WRITE_EN ? wea : 4'b0;
-wire [3:0] wen_b = WRITE_EN ? web : 4'b0;
+localparam BRAM_DATA_WIDTH = 32    ;
+localparam BRAM_BYTE_WIDTH = 8     ;
+localparam NB_COL          = BRAM_DATA_WIDTH/BRAM_BYTE_WIDTH;
+localparam BRAM_DEPTH      = DEPTH / NB_COL;
+localparam BRAM_ADDR_WIDTH = LW;
 
-wire [15:0] addra_idx = addra[LW-1:2];
-wire [15:0] addrb_idx = addrb[LW-1:2];
+wire [BRAM_ADDR_WIDTH-1:0] addra_idx = addra[BRAM_ADDR_WIDTH-1:2];
+wire [BRAM_ADDR_WIDTH-1:0] addrb_idx = addrb[BRAM_ADDR_WIDTH-1:2];
 
-generate if(DEPTH == 1024) begin // 1Kb
+reg [(NB_COL*BRAM_BYTE_WIDTH)-1:0] BRAM [BRAM_DEPTH-1:0];
+reg [(NB_COL*BRAM_BYTE_WIDTH)-1:0] ram_data_a = {(NB_COL*BRAM_BYTE_WIDTH){1'b0}};
+reg [(NB_COL*BRAM_BYTE_WIDTH)-1:0] ram_data_b = {(NB_COL*BRAM_BYTE_WIDTH){1'b0}};
 
-BRAM_TDP_MACRO #(
-.BRAM_SIZE("36Kb"), // Target BRAM: "18Kb" or "36Kb" 
-.DEVICE("7SERIES"), // Target device: "7SERIES" 
-.DOA_REG(0),        // Optional port A output register (0 or 1)
-.DOB_REG(0),        // Optional port B output register (0 or 1)
-.INIT_A(36'h0000000),  // Initial values on port A output port
-.INIT_B(36'h00000000), // Initial values on port B output port
-.INIT_FILE (MEMH_FILE),
-.READ_WIDTH_A (32),   // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-.READ_WIDTH_B (32),   // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-.WRITE_MODE_A("WRITE_FIRST"), // "WRITE_FIRST", "READ_FIRST", or "NO_CHANGE" 
-.WRITE_MODE_B("WRITE_FIRST"), // "WRITE_FIRST", "READ_FIRST", or "NO_CHANGE" 
-.WRITE_WIDTH_A(32), // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-.WRITE_WIDTH_B(32)  // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-) BRAM_TDP_MACRO_inst (
-.DOA    (douta  ), // Output port-A data, width defined by READ_WIDTH_A parameter
-.DOB    (doutb  ), // Output port-B data, width defined by READ_WIDTH_B parameter
-.ADDRA  (addra_idx), // Input port-A address, width defined by Port A depth
-.ADDRB  (addrb_idx), // Input port-B address, width defined by Port B depth
-.CLKA   (clka   ), // 1-bit input port-A clock
-.CLKB   (clka   ), // 1-bit input port-B clock
-.DIA    (32'b0), // Input port-A data, width defined by WRITE_WIDTH_A parameter
-.DIB    (32'b0), // Input port-B data, width defined by WRITE_WIDTH_B parameter
-.ENA    (ena    ), // 1-bit input port-A enable
-.ENB    (enb    ), // 1-bit input port-B enable
-.REGCEA (ena    ), // 1-bit input port-A output register enable
-.REGCEB (enb    ), // 1-bit input port-B output register enable
-.RSTA   (rsta   ), // 1-bit input port-A reset
-.RSTB   (rsta   ), // 1-bit input port-B reset
-.WEA    (wen_a  ), // Input port-A write enable, width defined by Port A depth
-.WEB    (wen_b  )  // Input port-B write enable, width defined by Port B depth
-);
+assign douta = ram_data_a;
+assign doutb = ram_data_b;
 
-
-end else if(DEPTH == 65536) begin // 64Kb
-
-wire en_a_lo = ena && !addra[15];
-wire en_a_hi = ena &&  addra[15];
-
-wire en_b_lo = ena && !addra[15];
-wire en_b_hi = ena &&  addra[15];
-
-reg  route_a_lo;
-reg  route_b_lo;
-
-wire [35:0] dout_a_lo;
-wire [35:0] dout_b_lo;
-
-wire [35:0] dout_a_hi;
-wire [35:0] dout_b_hi;
-
-assign douta = route_a_lo ? dout_a_lo : dout_a_hi;
-assign doutb = route_b_lo ? dout_b_lo : dout_b_hi;
-
-always @(posedge clka) begin
-    if(rsta) begin
-        route_a_lo <= 1'b0;
-        route_b_lo <= 1'b0;
-    end else begin
-        route_a_lo <= en_a_lo;
-        route_b_lo <= en_b_lo;
+// The following code either initializes the memory values to a specified file or to all zeros to match hardware
+generate
+  if (MEMH_FILE != "") begin: use_init_file
+    initial begin
+        $display("Loading: %s", MEMH_FILE);
+        $readmemh(MEMH_FILE, BRAM, 0, BRAM_DEPTH-1);
     end
-end
+  end else begin: init_bram_to_zero
+    integer ram_index;
+    initial
+      for (ram_index = 0; ram_index < BRAM_DEPTH; ram_index = ram_index + 1)
+        BRAM[ram_index] = {(NB_COL*BRAM_BYTE_WIDTH){1'b0}};
+  end
+endgenerate
 
-//
-// LOW 32K of the 64K BRAM
-//
-BRAM_TDP_MACRO #(
-.BRAM_SIZE("36Kb"), // Target BRAM: "18Kb" or "36Kb" 
-.DEVICE("7SERIES"), // Target device: "7SERIES" 
-.DOA_REG(0),        // Optional port A output register (0 or 1)
-.DOB_REG(0),        // Optional port B output register (0 or 1)
-.INIT_A(36'h0000000),  // Initial values on port A output port
-.INIT_B(36'h00000000), // Initial values on port B output port
-.INIT_FILE (MEMH_FILE),
-.READ_WIDTH_A (32),   // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-.READ_WIDTH_B (32),   // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-.WRITE_MODE_A("WRITE_FIRST"), // "WRITE_FIRST", "READ_FIRST", or "NO_CHANGE" 
-.WRITE_MODE_B("WRITE_FIRST"), // "WRITE_FIRST", "READ_FIRST", or "NO_CHANGE" 
-.WRITE_WIDTH_A(32), // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-.WRITE_WIDTH_B(32)  // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-) BRAM_TDP_MACRO_inst_lo (
-.DOA    (dout_a_lo), // Output port-A data, width defined by READ_WIDTH_A parameter
-.DOB    (dout_b_lo), // Output port-B data, width defined by READ_WIDTH_B parameter
-.ADDRA  (addra_idx), // Input port-A address, width defined by Port A depth
-.ADDRB  (addrb_idx), // Input port-B address, width defined by Port B depth
-.CLKA   (clka   ), // 1-bit input port-A clock
-.CLKB   (clka   ), // 1-bit input port-B clock
-.DIA    ({2'b00,dina}), // Input port-A data, width defined by WRITE_WIDTH_A parameter
-.DIB    ({2'b00,dinb}), // Input port-B data, width defined by WRITE_WIDTH_B parameter
-.ENA    (en_a_lo), // 1-bit input port-A enable
-.ENB    (en_b_lo), // 1-bit input port-B enable
-.REGCEA (en_a_lo), // 1-bit input port-A output register enable
-.REGCEB (en_b_lo), // 1-bit input port-B output register enable
-.RSTA   (rsta   ), // 1-bit input port-A reset
-.RSTB   (rsta   ), // 1-bit input port-B reset
-.WEA    (wen_a  ), // Input port-A write enable, width defined by Port A depth
-.WEB    (wen_b  )  // Input port-B write enable, width defined by Port B depth
-);
+generate
+genvar i;
+   for (i = 0; i < NB_COL; i = i+1) begin: byte_write
+     always @(posedge clka)
+       if (ena)
+         if (wea[i]) begin
+           BRAM[addra_idx][(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH] <= dina[(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH];
+           ram_data_a[(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH] <= dina[(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH];
+         end else begin
+           ram_data_a[(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH] <= BRAM[addra_idx][(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH];
+         end
 
-//
-// HIGH 32K of the 64K BRAM
-//
-BRAM_TDP_MACRO #(
-.BRAM_SIZE("36Kb"), // Target BRAM: "18Kb" or "36Kb" 
-.DEVICE("7SERIES"), // Target device: "7SERIES" 
-.DOA_REG(0),        // Optional port A output register (0 or 1)
-.DOB_REG(0),        // Optional port B output register (0 or 1)
-.INIT_A(36'h0000000),  // Initial values on port A output port
-.INIT_B(36'h00000000), // Initial values on port B output port
-.INIT_FILE (MEMH_FILE),
-.READ_WIDTH_A (32),   // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-.READ_WIDTH_B (32),   // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-.WRITE_MODE_A("WRITE_FIRST"), // "WRITE_FIRST", "READ_FIRST", or "NO_CHANGE" 
-.WRITE_MODE_B("WRITE_FIRST"), // "WRITE_FIRST", "READ_FIRST", or "NO_CHANGE" 
-.WRITE_WIDTH_A(32), // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-.WRITE_WIDTH_B(32)  // Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
-) BRAM_TDP_MACRO_inst_hi (
-.DOA    (dout_a_hi), // Output port-A data, width defined by READ_WIDTH_A parameter
-.DOB    (dout_b_hi), // Output port-B data, width defined by READ_WIDTH_B parameter
-.ADDRA  (addra_idx), // Input port-A address, width defined by Port A depth
-.ADDRB  (addrb_idx), // Input port-B address, width defined by Port B depth
-.CLKA   (clka   ), // 1-bit input port-A clock
-.CLKB   (clka   ), // 1-bit input port-B clock
-.DIA    ({2'b00,dina}), // Input port-A data, width defined by WRITE_WIDTH_A parameter
-.DIB    ({2'b00,dinb}), // Input port-B data, width defined by WRITE_WIDTH_B parameter
-.ENA    (en_a_hi), // 1-bit input port-A enable
-.ENB    (en_b_hi), // 1-bit input port-B enable
-.REGCEA (en_a_hi), // 1-bit input port-A output register enable
-.REGCEB (en_b_hi), // 1-bit input port-B output register enable
-.RSTA   (rsta   ), // 1-bit input port-A reset
-.RSTB   (rsta   ), // 1-bit input port-B reset
-.WEA    (wen_a  ), // Input port-A write enable, width defined by Port A depth
-.WEB    (wen_b  )  // Input port-B write enable, width defined by Port B depth
-);
-
-end endgenerate
+     always @(posedge clka)
+       if (enb)
+         if (web[i]) begin
+           BRAM[addrb_idx][(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH] <= dinb[(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH];
+           ram_data_b[(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH] <= dinb[(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH];
+         end else begin
+           ram_data_b[(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH] <= BRAM[addrb_idx][(i+1)*BRAM_BYTE_WIDTH-1:i*BRAM_BYTE_WIDTH];
+         end
+   end
+endgenerate
 
 endmodule
 
