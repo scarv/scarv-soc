682312f5c40d597a4107b1c0ced81c1d03bd6879
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-10-31 11:14:49 +0000

    Use GPIOs in the UART example

--- src/examples/uart/example_uart.c
+++ src/examples/uart/example_uart.c
@@ -8,10 +8,14 @@ char * finish_message = "\nRecieved Finish Byte. Stop.\n";
 @brief A simple test program which echoes recieved UART bytes.
 */
 int main(int argc, char ** argv) {
+
+    scarvsoc_uart_getc_b(SCARVSOC_UART0);
     
     scarvsoc_uart_putstr_b(SCARVSOC_UART0, start_message);
     
     char recv = scarvsoc_uart_getc_b(SCARVSOC_UART0);
+
+    uint32_t counter = 0;
 
     while(recv != '!') {
 
@@ -19,9 +23,15 @@ int main(int argc, char ** argv) {
 
         recv = scarvsoc_uart_getc_b(SCARVSOC_UART0);
 
+        counter ++;
+
+        scarvsoc_gpio_wr(SCARVSOC_GPIO1, counter);
+
     }
 
     scarvsoc_uart_putstr_b(SCARVSOC_UART0, finish_message);
+    
+    scarvsoc_gpio_wr(SCARVSOC_GPIO1, 0);
 
     return 0;
 }
