1fc082643f15fd95cb4976486e477954ef90e8e7
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-04-27 15:28:33 +0100

    Flow: Re-use scarv cpu RTL manifest.
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   bin/conf.sh
    	modified:   extern/scarv-cpu
    	modified:   flow/synthesis/scarv-soc-rtl.tcl
    	modified:   flow/verilator/Makefile.in
    	modified:   flow/verilator/scarv-soc-rtl.manifest

--- bin/conf.sh
+++ bin/conf.sh
@@ -4,7 +4,10 @@ export SOC_HOME="${PWD}"
 export SOC_WORK=$SOC_HOME/work
 export TEXMFLOCAL="${TEXMFLOCAL}:${SOC_HOME}/extern/texmf"
 
-export SCARV_CPU_RTL_DIR=$SOC_HOME/extern/scarv-cpu/rtl/core
+export SCARV_CPU=$SOC_HOME/extern/scarv-cpu
+export FRV_HOME=$SCARV_CPU
+export XCRYPTO_RTL=$SCARV_CPU/external/xcrypto/rtl
+
 
 if [[ -z "$VERILATOR_ROOT" ]]; then
     echo "Warning: No VERILATOR_ROOT environment variable set."
@@ -19,6 +22,6 @@ echo "SOC_HOME          = $SOC_HOME"
 echo "SOC_WORK          = $SOC_WORK"
 echo "VERILATOR_ROOT    = $VERILATOR_ROOT"
 echo "YOSYS_ROOT        = $YOSYS_ROOT"
-echo "SCARV_CPU_RTL_DIR = $SCARV_CPU_RTL_DIR"
+echo "SCARV_CPU         = $SCARV_CPU"
 echo "-----------------------------------------------------------------------"
 
--- flow/synthesis/scarv-soc-rtl.tcl
+++ flow/synthesis/scarv-soc-rtl.tcl
@@ -15,40 +15,40 @@ read_verilog $::env(SOC_HOME)/rtl/rng/scarv_rng_top.v
 
 #
 # XCrypto + SCARV CPU sources.
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/p_addsub/p_addsub.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/p_shfrot/p_shfrot.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_sha3/xc_sha3.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_sha256/xc_sha256.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_aessub/xc_aessub.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_aessub/xc_aessub_sbox.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_aesmix/xc_aesmix.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_malu/xc_malu.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_malu/xc_malu_divrem.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_malu/xc_malu_long.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_malu/xc_malu_mul.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_malu/xc_malu_pmul.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/xc_malu/xc_malu_muldivrem.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/b_bop/b_bop.v
-read_verilog $::env(SOC_HOME)/extern/scarv-cpu/external/xcrypto-rtl/rtl/b_lut/b_lut.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_alu.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_asi.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_bitwise.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_core.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_core_fetch_buffer.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_counters.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_csrs.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_gprs.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_interrupts.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_leak.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_lsu.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_pipeline.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_pipeline_decode.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_pipeline_execute.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_pipeline_fetch.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_pipeline_memory.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_pipeline_register.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_pipeline_writeback.v
-read_verilog $::env(SCARV_CPU_RTL_DIR)/frv_rngif.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/p_addsub/p_addsub.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/p_shfrot/p_shfrot.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_sha3/xc_sha3.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_sha256/xc_sha256.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_aessub/xc_aessub.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_aessub/xc_aessub_sbox.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_aesmix/xc_aesmix.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_malu/xc_malu.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_malu/xc_malu_divrem.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_malu/xc_malu_long.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_malu/xc_malu_mul.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_malu/xc_malu_pmul.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/xc_malu/xc_malu_muldivrem.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/b_bop/b_bop.v
+read_verilog $::env(SCARV_CPU)/external/xcrypto/rtl/b_lut/b_lut.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_alu.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_asi.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_bitwise.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_core.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_core_fetch_buffer.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_counters.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_csrs.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_gprs.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_interrupts.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_leak.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_lsu.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_pipeline.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_pipeline_decode.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_pipeline_execute.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_pipeline_fetch.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_pipeline_memory.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_pipeline_register.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_pipeline_writeback.v
+read_verilog $::env(SCARV_CPU)/rtl/core/frv_rngif.v
 
 
 #
