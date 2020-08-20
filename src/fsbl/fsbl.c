1ff4cf1622ed4d36e420cbb71f5e7f8e7cb9c4e8
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-08-20 16:29:37 +0100

    skw: fsbl: Simple panic handler.
    - Dump most important reigster state.
    
     On branch scarv/skywater/dev
     Your branch is ahead of 'origin/scarv/skywater/dev' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   src/fsbl/boot.S
    	modified:   src/fsbl/fsbl.c

--- src/fsbl/fsbl.c
+++ src/fsbl/fsbl.c
@@ -5,12 +5,6 @@
     #define UART_BASE 0x10000000
 #endif
 
-#ifndef GPIO_BASE
-    #define GPIO_BASE 0x10001000
-#endif
-
-#define GPIO_LEDS 2
-
 #define UART_RX 0
 #define UART_TX 1
 #define UART_ST 2
@@ -18,9 +12,6 @@
 
 //! Pointer to the UART register space.
 static volatile uint32_t * uart = (volatile uint32_t*)(UART_BASE);
-
-//! Pointer to the GPIO register space.
-static volatile uint32_t * gpio = (volatile uint32_t*)(GPIO_BASE);
 
 const uint32_t UART_CTRL_RST_TX_FIFO = 0x00000001;
 const uint32_t UART_CTRL_RST_RX_FIFO = 0x00000002;
@@ -40,6 +31,9 @@ uint8_t uart_rd_char(){
 //! Jump to the main function
 extern void __fsbl_goto_main(uint32_t * tgt);
 
+//! The very first thing executed.
+extern void __fsbl_start();
+
 /*!
 @brief Setup the UART peripheral
 @details Configures the UART peripheral such that:
@@ -47,23 +41,30 @@ extern void __fsbl_goto_main(uint32_t * tgt);
 - Interrupts are disabled
 */
 void fsbl_uart_setup() {
-
     uart[UART_CT] = UART_CTRL_RST_TX_FIFO | UART_CTRL_RST_RX_FIFO ;
-
 }
 
 /*!
 @brief Print the simple welcome message to show we are ready.
 */
 void putstr(char * msg) {
-
     for(int i = 0; msg[i]; i ++) {
-        
         while(uart[UART_ST] & UART_STATUS_TX_FULL) {
             // Do nothing.
         }   
         uart[UART_TX] = msg[i];
+    }
+}
 
+
+//! Print a 32-bit number as hex
+void puthex32(uint32_t w) {
+    char lut []= "0123456789ABCDEF";
+    for(int i =  3; i >= 0; i --) {
+        char b[3];
+        b[1]= lut[(w >> (8*i    )) & 0xF];
+        b[0]= lut[(w >> (8*i + 4)) & 0xF];
+        putstr(b);
     }
 }
 
@@ -77,17 +78,10 @@ void putstr(char * msg) {
     memory segment, then jumps to the RAM.
 */
 void fsbl() {
-    
-
-    gpio[GPIO_LEDS] = 0x1;
 
     fsbl_uart_setup();
-    
-    gpio[GPIO_LEDS] = 0x2;
 
     putstr("scarv-soc fsbl\n");
-    
-    gpio[GPIO_LEDS] = 0x4;
     
     // First 4 bytes are the size of the program (in bytes).
     uint32_t    program_size =
@@ -95,8 +89,6 @@ void fsbl() {
         ((uint32_t)uart_rd_char() << 16) |
         ((uint32_t)uart_rd_char() <<  8) |
         ((uint32_t)uart_rd_char() <<  0) ;
-    
-    gpio[GPIO_LEDS] = 0x8;
 
     // Next 4 bytes are a 32-bit destination address.
     uint32_t    program_dest =
@@ -104,34 +96,46 @@ void fsbl() {
         ((uint32_t)uart_rd_char() << 16) |
         ((uint32_t)uart_rd_char() <<  8) |
         ((uint32_t)uart_rd_char() <<  0) ;
-    
-    gpio[GPIO_LEDS] = -1;
-
-    int bytes_per_led = program_size / 8;
-    int led_count     = 0;
 
     uint8_t * dest_ptr = (uint8_t*)program_dest;
 
     // Download the program and write it to the destination memory.
     for(uint32_t i = 0; i < program_size; i ++) {
-
-        led_count += 1;
-        
         dest_ptr[i] = uart_rd_char();
-
-        if(led_count >= bytes_per_led) {
-            led_count = 0;
-
-            gpio[GPIO_LEDS] = gpio[GPIO_LEDS] << 1;
-        }
-
     }
-    
-    gpio[GPIO_LEDS] = 0x0;
     
     putstr("boot\n");
 
     // Jump to the downloaded program.
     __fsbl_goto_main((uint32_t*)program_dest);
 
+}
+
+/*!
+@brief FSBL panic handler.
+@details Prints register state, then jumps back to __fsbl_start
+*/
+void panic() {
+    putstr("\npanic!\n");
+    uint32_t mepc, mstatus, mtval, mcause, sp, ra;
+    
+    asm volatile("csrr %0, mepc     " : "=r"(mepc   ));
+    puthex32(mepc   );putstr("\n");    
+    
+    asm volatile("csrr %0, mstatus  " : "=r"(mstatus));
+    puthex32(mstatus);putstr("\n");    
+    
+    asm volatile("csrr %0, mtval    " : "=r"(mtval  ));
+    puthex32(mtval  );putstr("\n");    
+    
+    asm volatile("csrr %0, mcause   " : "=r"(mcause ));
+    puthex32(mcause );putstr("\n");    
+    
+    asm volatile("mv   %0, sp       " : "=r"(sp     ));
+    puthex32(sp     );putstr("\n");    
+    
+    asm volatile("mv   %0, ra       " : "=r"(ra     ));
+    puthex32(ra     );putstr("\n");    
+
+    __fsbl_start();   
 }
